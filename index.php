8542e0fc181f7092596c8a618d6d6191f40da3a4
unknown <jtrembl1@ROL-MKTG2710.rollins.local>
2016-03-22 11:12:27 -0400

    Trying to require the file that is erroring out.

--- index.php
+++ index.php
@@ -1,29 +1,5 @@
 <?php
 
-require_once 'azure\WindowsAzure\WindowsAzure.php';
-use WindowsAzure\Common\ServicesBuilder;
-
-$connectionString = "DefaultEndpointsProtocol=https;AccountName=vanman;AccountKey=QdWBBF/0E+rYpBrk5YC0kyV7CxRgnP9CP0AhQG4Q9R8cDIFIbIyHHwoK3I+GgAlfOb4V7ifiDZ6BRBDsGvefIQ==";
-
-// Create blob REST proxy.
-$blobRestProxy = ServicesBuilder::getInstance()->createBlobService($connectionString);
-
-
-try {
-    // List blobs.
-    $blob_list = $blobRestProxy->listBlobs("mycontainer");
-    $blobs = $blob_list->getBlobs();
-
-    foreach($blobs as $blob)
-    {
-        echo $blob->getName().": ".$blob->getUrl()."<br />";
-    }
-}
-catch(ServiceException $e){
-    // Handle exception based on error codes and messages.
-    // Error codes and messages are here:
-    // http://msdn.microsoft.com/library/azure/dd179439.aspx
-    $code = $e->getCode();
-    $error_message = $e->getMessage();
-    echo $code.": ".$error_message."<br />";
-}
+	require( 'D:\home\site\wwwroot\azure\WindowsAzure\Common\Internal\Http\HttpClient.php' );
+	
+	echo "Requiring"
