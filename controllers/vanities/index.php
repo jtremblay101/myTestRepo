fe669de6dbe0223550ce40fed614c0ab364c26be
unknown <jtrembl1@ROL-MKTG2710.rollins.local>
2016-03-24 13:16:46 -0400

    Handler returning errors for testing

--- controllers/vanities/index.php
+++ controllers/vanities/index.php
@@ -1,7 +1,22 @@
 <?
 if(isset($_POST["action"]))
 {
-	echo json_encode([
-		"data" => $_POST["data"]
-		]);
+	$action = $_POST["action"];
+	if($action == "create")
+	{
+		echo json_encode([
+			"data" => $_POST["data"]
+			]);			
+	}
+	elseif($action == "edit")
+	{
+		$error = [
+			"error" => "There was a problem editing your stuff."
+		]
+		echo json_encode($error);
+	}
+	else
+	{
+		echo {};
+	}
 }
