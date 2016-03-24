1b1569a915addeee1454006f12cdc531d278c1a4
unknown <jtrembl1@ROL-MKTG2710.rollins.local>
2016-03-24 10:29:03 -0400

    Added isset function to functions.js and fixed the data variable which is still assigning each value to an array.

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -323,6 +323,22 @@ function json_decode(str_json) {
 	return null;
 }
 
+function isset() {
+	var a = arguments, l = a.length, i = 0, undef;
+
+	if (l === 0) {
+		throw new Error('Empty isset');
+	}
+
+	while (i !== l) {
+		if (a[i] === undef || a[i] === null) {
+			return false;
+		}
+		i++;
+	}
+	return true;
+}
+
 
 //////////////////////////////////////////////////
 // All Force functions after this line!		//
