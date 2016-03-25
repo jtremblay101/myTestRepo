487654a9989a2c26f990d24088e8666b375e4a1a
unknown <jtrembl1@ROL-MKTG2710.rollins.local>
2016-03-25 07:24:16 -0400

    Trying array values as suggested by allen.

--- controllers/vanities/index.php
+++ controllers/vanities/index.php
@@ -10,9 +10,9 @@ if(isset($_POST["action"]))
 	}
 	elseif($action == "edit")
 	{
-		echo '
-			{"data":{"0":{"Row Number":"1","PartitionKey":"","RowKey":"44pestthrea","Timestamp":"06-12-2012 06:06:16 pm","Options":"0","Destination":"http:\/\/www.pestthreat.com"}}}
-		';	
+		echo json_encode([
+			"data" => array_values($_POST["data"]);
+			]);		
 	}
 	else
 	{
